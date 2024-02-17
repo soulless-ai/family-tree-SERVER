fa1154c1edab9f01b5beab49ee4308abe10128b8
soulless-ai <mr.chupak@gmail.com>
2024-02-17 18:59:17 +0500

    update port

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 export default {
     serverDomain: 'server-hk59.onrender.com',
-    serverPort: process.env.PORT || 3000,
+    serverPort: 3000,
     poolClients: {
         user: 'db_ivgy_user',
         host: 'dpg-cn84h9ol5elc738r3c70-a.oregon-postgres.render.com',
-        database: 'db_ivgy  ',
+        database: 'db_ivgy',
         password: 'KK7fvyHyTFtKIN00l5UAIeX7FEJYk3gj',
         port: 5432,
         ssl: {
